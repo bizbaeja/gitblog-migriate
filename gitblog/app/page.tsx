677b769f013327843e86b9455e38a462cd2e7a8e
bizbaeja <bjgbjg112@gmail.com>
2024-01-11 15:53:07 +0900

    auth & themes [#2]

--- gitblog/app/page.tsx
+++ gitblog/app/page.tsx
@@ -1,9 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
- 
-export default function Home() {
+import { UserButton } from "@clerk/nextjs"
+
+export default function Page() {
   return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton 
+      afterSignOutUrl="/"
+      />
     </div>
+
   )
 }
