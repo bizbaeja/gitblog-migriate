9ab5ba68b0e57f24c67bc779510c3348684a1626
bizbaeja <bjgbjg112@gmail.com>
2024-01-19 12:40:16 +0900

    github logo

--- gitblog/components/portfolio.tsx
+++ gitblog/components/portfolio.tsx
@@ -11,13 +11,20 @@ const Portfolio: NextPage = () => {
         <Carousel>
           {data.map((data, i) => (
             <div className="relative h-96 w-96 flex-[0_0_100%]" key={i}>
-              <Link href={data.link}>
                 <Image alt="alt" fill className="h-full w-full object-cover opacity-35" src={data.src} />
-              <div className="hover:text-green-400 absolute inset-0 flex flex-col justify-center items-center">
-                <h2 className="text-xl font-bold text-center">{data.name}</h2>
-                <p className="text-muted-foreground text-center">{data.desc}</p>
-              </div>
+              <div className=" absolute inset-0 flex flex-col justify-center items-center">
+              <Link href={data.link}>
+                <h2 className="hover:text-green-400 text-xl font-bold text-center">{data.name}</h2>
               </Link>
+                <p className="text-muted-foreground text-center">{data.desc}</p>
+                <div className="flex">
+                    <Link href={data.github} >
+                        <Image alt="github" src="/github.svg" width={30} height={30}  style={{backgroundColor:"white", borderRadius:"50%"}}/>
+                    </Link>
+                      
+                </div>
+             
+              </div>
             </div>
           ))}
         </Carousel>
